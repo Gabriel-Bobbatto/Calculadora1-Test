0a7b13f4d477106befa72306b57e27212fea5fab
Gabriel-Bobbatto <gabrielbobbatto@gmail.com>
2022-06-23 14:37:11 -0300

    final

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,11 +12,9 @@ namespace Calculadora
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
         private void button6_Click(object sender, EventArgs e)
         {
             // n1
@@ -26,7 +24,6 @@ namespace Calculadora
             Pantalla.Text += "1";
 
         }
-
         private void button7_Click(object sender, EventArgs e)
         {
             //n2
@@ -34,7 +31,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "2";
         }
-
         private void button8_Click(object sender, EventArgs e)
         {
             if (Pantalla.Text == "0")
@@ -42,7 +38,6 @@ namespace Calculadora
             Pantalla.Text += "3";
             //n3
         }
-
         private void button10_Click(object sender, EventArgs e)
         {
             //n4
@@ -50,7 +45,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "4";
         }
-
         private void button11_Click(object sender, EventArgs e)
         {
             //n5
@@ -58,7 +52,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "5";
         }
-
         private void button12_Click(object sender, EventArgs e)
         {
             //n6
@@ -66,7 +59,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "6";
         }
-
         private void button14_Click(object sender, EventArgs e)
         {
             //n7
@@ -74,7 +66,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "7";
         }
-
         private void button15_Click(object sender, EventArgs e)
         {
             //n8
@@ -82,7 +73,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "8";
         }
-
         private void button16_Click(object sender, EventArgs e)
         {
             //n9
@@ -90,7 +80,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "9";
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             //n0
@@ -98,7 +87,6 @@ namespace Calculadora
                 Pantalla.Text = "";
             Pantalla.Text += "0";
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
             // coma ,
@@ -107,13 +95,10 @@ namespace Calculadora
                 Pantalla.Text += ",";
             }
         }
-
         private void button5_Click(object sender, EventArgs e)
         {
             // = igual
             valor2 = Convert.ToDouble(Pantalla.Text);
-
-            resultado = valor1 + valor2;
             switch (operacion)
             {
                 case 1:
@@ -139,10 +124,8 @@ namespace Calculadora
                     resultado = (valor1 * valor2) / 100;
                     break;
             }
-
             Pantalla.Text = resultado.ToString();
         }
-
         private void button20_Click(object sender, EventArgs e)
         {
             // / dividir
@@ -150,9 +133,6 @@ namespace Calculadora
             valor1 = Convert.ToDouble(Pantalla.Text);
             Pantalla.Text = "0";
         }
-            
-        
-
         private void button17_Click(object sender, EventArgs e)
         {
             // x multiplicar
@@ -160,7 +140,6 @@ namespace Calculadora
             valor1 = Convert.ToDouble(Pantalla.Text);
             Pantalla.Text = "0";
         }
-
         private void button13_Click(object sender, EventArgs e)
         {
             // menos
@@ -168,7 +147,6 @@ namespace Calculadora
             valor1 = Convert.ToDouble(Pantalla.Text);
             Pantalla.Text = "0";
         }
-
         private void button9_Click(object sender, EventArgs e)
         {
             // mas
@@ -176,7 +154,6 @@ namespace Calculadora
             valor1 = Convert.ToDouble(Pantalla.Text);
             Pantalla.Text = "0";
         }
-
         private void button18_Click(object sender, EventArgs e)
         {
             // C borrar
@@ -185,21 +162,18 @@ namespace Calculadora
             resultado = 0;
             Pantalla.Text = "0";
         }
-
         private void button19_Click(object sender, EventArgs e)
         {
             operacion = 5;
             valor1 = Convert.ToDouble(Pantalla.Text);
             Pantalla.Text = "0";
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             signos = double.Parse(Pantalla.Text);
             signos = signos - (signos * 2);
             Pantalla.Text = signos.ToString();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if (Pantalla.Text.Length > 1)
